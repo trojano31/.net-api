eca5df4fd49c235ad62badfb07238812e927a250
trojan <patryk.trojanowski98@gmail.com>
2019-10-06 15:02:27 +0200

    initial

---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace WebApplication2.Controllers
+{
+
+  public class Author
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+  }
+
+  public class Blog
+  {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public Author Author { get; set; }
+    public ICollection<Post> Posts { get; set; }
+
+    public Blog(Guid id, string name, DateTime createdOn, Author author, ICollection<Post> posts = null)
+    {
+      Id = id;
+      Name = name;
+      CreatedOn = createdOn;
+      Author = author;
+      Posts = posts ?? new List<Post>();
+    }
+  }
+
+  public class Post
+  {
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedOn { get; set; }
+  }
+
+  public class CreateBlogRequest
+  {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public Author Author { get; set; }
+  }
+  
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BlogController : ControllerBase
+  {
+    private readonly IMongoCollection<Blog> _collection;
+
+    public BlogController()
+    {
+      var client = new MongoClient("mongodb://localhost:27017");
+      var database = client.GetDatabase("blog");
+      _collection = database.GetCollection<Blog>("blog");
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+      return new string[] {"value1", "value2"};
+    }
+    
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+      return "value";
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] CreateBlogRequest blogRequest)
+    {
+      if (blogRequest.CreatedOn < DateTime.Now.AddYears(-1))
+      {
+        return BadRequest();
+      };
+      
+      var blog = new Blog(
+        blogRequest.Id,
+        blogRequest.Name,
+        blogRequest.CreatedOn,
+        blogRequest.Author
+      );
+
+      await _collection.InsertOneAsync(blog);
+      return Ok();
+    }
+    
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
---
+++ WebApplication2/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson.Serialization;
+using WebApplication2.Controllers;
+
+namespace WebApplication2
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      BsonClassMap.RegisterClassMap<Blog>(x =>
+      {
+        x.AutoMap();
+        x.SetIgnoreExtraElements(true);
+        x.MapIdMember(y => y.Id);
+      });
+      
+      BsonClassMap.RegisterClassMap<Post>(x =>
+      {
+        x.AutoMap();
+        x.SetIgnoreExtraElements(true);
+      });
+      
+      BsonClassMap.RegisterClassMap<Author>(x =>
+      {
+        x.AutoMap();
+        x.SetIgnoreExtraElements(true);
+      });
+      
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseHsts();
+      }
+
+      // app.UseHttpsRedirection();
+      app.UseMvc();
+    }
+  }
+}
